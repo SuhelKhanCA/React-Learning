dc9ed59360af6a21b4c4320b9d8be02132583076
suhelkhanca <suhel.hata@gmail.com>
2024-12-26 13:36:28 +0530

    add : creating a simple react app using CRA

--- 01-react-fundamental/02-create-react-app/my-first-app/src/index.js
+++ 01-react-fundamental/02-create-react-app/my-first-app/src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+function Hello() {
+  return <h1>Hello World !</h1>
+}
+
+// const rootElement = document.getElementById("root");
+
+// ReactDOM.createRoot(rootElement).render(<h1>Suhel Khan</h1>)
+
+// ReactDOM.createRoot(document.getElementById("root")).render(<Hello/>);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <h1>Hello World ... !</h1>
   </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
