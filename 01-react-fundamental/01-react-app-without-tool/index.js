9242c7f2c47c2205a83d6b8a9ba616200ead4fd8
suhelkhanca <suhel.hata@gmail.com>
2025-01-20 21:25:16 +0530

    creating react app from scratch without using any Bundler

--- 01-react-fundamental/01-react-app-without-tool/index.js
+++ 01-react-fundamental/01-react-app-without-tool/index.js
@@ -1,3 +1,4 @@
+// creating React app without CRA or VITE
 
 const rootElement = document.getElementById("root");
 
@@ -12,28 +13,28 @@ const el = React.createElement("h1", null, "Hello world!");
 
 console.log(el); // obj
 
+// put 'el' to DOM as child of 'rootElement'
 // ReactDOM.createRoot(rootElement).render(el);
 // ReactDOM.createRoot(rootElement).render(Hello());
-
 
 // JSX
 
 function HelloWorld() {
-  return <h1 className="heading">Hello World</h1>
+  return <h1 className="heading">Hello World</h1>;
 }
 // h1, p, div, article, main
 // ReactDOM.createRoot(rootElement).render(<HelloWorld />);
 
-
 // Things to remember
-
 // react, reactDOM
 // JSX(Babel convert jsx to actual react code)
 
+
+// Summary
 const rootEle = document.getElementById("root");
 
 function Hello() {
-    return <h1 className="heading">Hello Suhel into the world of React</h1>
+  return <h1 className="heading">Hello Suhel into the world of React</h1>;
 }
 
-ReactDOM.createRoot(rootEle).render(<Hello/>)
+ReactDOM.createRoot(rootEle).render(<Hello />);
