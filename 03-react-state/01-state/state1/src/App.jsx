adbeb48aba8873d152236743bc737427b378b8b2
suhelkhanca <suhel.hata@gmail.com>
2025-01-30 17:19:22 +0530

    react state : useState hook

---
+++ 03-react-state/01-state/state1/src/App.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import "./App.css";
+import Counter from "./Counter";
+
+// hooks:
+// class based component : old way
+// useState
+// useEffect
+
+// We can't use normal vars to change the UI
+
+// state
+// state stores --> data
+// when state change => re-render the UI
+
+
+// useState : a hook
+// useState(defaultValue)
+// returns : [stateValue, functionToUpdateThatStateValue]
+
+
+function App() {
+  // let firstName = "John";
+
+  // function changeName() { // var is changed but UI does not changed
+  //   console.log(firstName);
+  //   firstName = "Suhel";
+  //   console.log(firstName);
+  // }
+
+
+  // state ---> "Harshit"
+  // state chage ---> "Suhel"
+
+  // when state changes, our component re-renders
+  // then UI changes
+
+  // const returnedValue = useState("Harshit");
+  // const firstName = returnedValue[0];
+  // const functionToUpdateFirstName = returnedValue[1];
+
+  const [firstName, setFirstName] = useState("Harshit");
+  // wrapper
+  function handleNameChange() {
+    setFirstName("Suhel");
+  }
+  // return (
+  //   <>
+  //     <h1>{firstName}</h1>
+  //     <button onClick={handleNameChange}>Click me</button>
+  //   </>
+  // );
+
+  return (
+    <>
+      <Counter />
+    </>
+  )
+}
+
+export default App;
