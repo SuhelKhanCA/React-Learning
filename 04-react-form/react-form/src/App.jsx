c424c28845c3d4a2f0cee977d3ef6c6a69cb5854
Suhel Khan <112371384+SuhelKhanCA@users.noreply.github.com>
2025-02-03 09:01:02 +0000

    form with username, password and button

--- 04-react-form/react-form/src/App.jsx
+++ 04-react-form/react-form/src/App.jsx
@@ -5,17 +5,37 @@ import "./App.css";
 
 function App() {
   const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   return (
     <>
-      <div>
+      <div id="root">
         <h1>Form Basics</h1>
         <form>
           <div>
-            <label htmlFor="username">UserName</label> 
-            <input type="text" id="username" value={username} onChange={(e) => {
-              setUsername(e.target.value);
-            } } />
+            <label htmlFor="username">UserName</label>
+            <input
+              type="text"
+              id="username"
+              value={username}
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+            <div>
+              <button>Submit</button>
+            </div>
           </div>
         </form>
       </div>
