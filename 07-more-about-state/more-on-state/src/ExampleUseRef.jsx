d996d48fe3af59cad2f51e214a4ede2e68aea959
suhelkhanca <suhel.hata@gmail.com>
2025-02-07 19:17:58 +0530

    useRef hooks and its behavior

--- 07-more-about-state/more-on-state/src/ExampleUseRef.jsx
+++ 07-more-about-state/more-on-state/src/ExampleUseRef.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useRef } from "react";
 import { useState } from "react";
 
 // custom hooks
@@ -9,23 +10,63 @@ import { useState } from "react";
 function ExampleUseRef() {
   console.log("Component rendered");
 
-  const [counter, setCounter] = useState(0);
-  const [username, setUsername] = useState("Suhel");
+  //   const [counter, setCounter] = useState(0);
+  //   const [username, setUsername] = useState("Suhel");
+
+  //   return (
+  //     <div>
+  //       {/* <h1>{counter}</h1>
+  //       <button
+  //         onClick={() => {
+  //           setCounter(counter + 1);
+  //         }}
+  //       >
+  //         Increase Counter
+  //       </button>
+  //           <h4>{username}</h4>
+  //           <button onClick={() => {
+  //               setUsername("Monu");
+  //           }}>Change Name</button> */}
+
+  //     </div>
+  //   );
+
+  //   const username = useRef("Suhel");
+  //   console.log(username);
+
+  //     return (
+  //       <>
+  //             <h4>{username.current}</h4>
+  //             <button onClick={() => {
+  //                 console.log("username changed")
+  //                 username.current = "Monu";
+  //             }}>Change Name</button>
+  //       </>
+  //     );
+
+  // userRef
+  // string
+  // number
+  // []
+  // {}
+    // html element
+    
+    // learning : Change of ref does not trigger re-render of html element
+  const h1Ref = useRef();
+  function handleClick() {
+    // console.log(h1Ref);
+    console.log(h1Ref.current);
+    const h1 = h1Ref.current;
+    h1.textContent = "This is new text";
+      h1.style.background = "blue";
+    console.log(h1);
+  }
+
   return (
-    <div>
-      <h1>{counter}</h1>
-      <button
-        onClick={() => {
-          setCounter(counter + 1);
-        }}
-      >
-        Increase Counter
-      </button>
-          <h4>{username}</h4>
-          <button onClick={() => {
-              setUsername("Monu");
-          }}>Change Name</button>
-    </div>
+    <>
+      <h1 ref={h1Ref}>Hello There</h1>
+      <button onClick={handleClick}>Change value</button>
+    </>
   );
 }
 
