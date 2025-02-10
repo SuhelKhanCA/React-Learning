a88276d25eabb8ca3d8e6077111e2b2ba5afb49b
suhelkhanca <suhel.hata@gmail.com>
2025-02-10 22:17:34 +0530

    add : initial setup for todo-app using useReducer

---
+++ 09-reducer-and-context/02-todo-app-using-reducer/todo-app/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import { useReducer } from 'react';
+
+
+
+function App() {
+const [todos, dispatch] = useReducer(useReducer, initialTodos)
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
