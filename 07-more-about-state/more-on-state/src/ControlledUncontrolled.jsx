b4ce5c4e168a60c7a0de6f5864c19786184a85fc
suhelkhanca <suhel.hata@gmail.com>
2025-02-10 22:23:12 +0530

    use ref example

--- 07-more-about-state/more-on-state/src/ControlledUncontrolled.jsx
+++ 07-more-about-state/more-on-state/src/ControlledUncontrolled.jsx
@@ -5,21 +5,30 @@ import {useRef} from 'react'
 function ControlledUncontrolled() {
     // Example useRef()
     const usernameRef = useRef();
+    const passwordRef = useRef();
     function handleSubmit(e) {
         e.preventDefault();
-        console.log(usernameRef);
+        console.log(usernameRef.current);
+        console.log(usernameRef.current.value);
+
+        console.log(passwordRef.current);
+        console.log(passwordRef.current.value);
     }
   return (
-      <div>
-          <form onSubmit={handleSubmit}>
-              <label htmlFor='username'>Username</label>
-              <br />
-              <input type="text" id='username' ref={ usernameRef } />
-              <br />
-              <button>Submit</button>
-          </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Username</label>
+        <br />
+        <input type="text" id="username" ref={usernameRef} />
+        <br />
+        <label htmlFor="password">Password</label>
+        <br />
+        <input type="password" id="password" ref={passwordRef} />
+        <br />
+        <button>Submit</button>
+      </form>
     </div>
-  )
+  );
 }
 
 export default ControlledUncontrolled;
