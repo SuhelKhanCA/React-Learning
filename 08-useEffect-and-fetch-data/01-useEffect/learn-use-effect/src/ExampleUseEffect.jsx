881127e953cdc08bf9737579cf76e737fa792d11
suhelkhanca <suhel.hata@gmail.com>
2025-03-11 03:39:46 +0530

    useEffect example

--- 08-useEffect-and-fetch-data/01-useEffect/learn-use-effect/src/ExampleUseEffect.jsx
+++ 08-useEffect-and-fetch-data/01-useEffect/learn-use-effect/src/ExampleUseEffect.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 // useState
 
@@ -7,10 +7,8 @@ import React, { useEffect, useState } from 'react'
 // eventListeners
 // side effects
 
-
 // class based component : Not used no
 // react life cycle methods -> (componentDidMount)
-
 
 // useEffect
 // it takes callback function as input
@@ -21,20 +19,25 @@ import React, { useEffect, useState } from 'react'
 // dependency array with some value : On changes, useEffect re-rendered
 
 function ExampleUseEffect() {
-    const [counter, setCounter] = useState(0);
-    console.log("component rendered");
-    useEffect(() => { // every time get rendered on component rendering
-        console.log("Inside use effect!");
-    }, [counter])
+  const [counter, setCounter] = useState(0);
+  console.log("component rendered");
+  useEffect(() => {
+    // every time get rendered on component rendering
+    console.log("Inside use effect!");
+  }, [counter]);
   return (
-      <div>
-          <h2>On component rendering : useEffect</h2>
-          <h3>{counter}</h3>
-          <button onClick={() => {
-              setCounter(counter + 1);
-          }}>Increase</button>
+    <div>
+      <h2>On component rendering : useEffect</h2>
+      <h3>{counter}</h3>
+      <button
+        onClick={() => {
+          setCounter(counter + 1);
+        }}
+      >
+        Increase
+      </button>
     </div>
-  )
+  );
 }
 
 export default ExampleUseEffect;
