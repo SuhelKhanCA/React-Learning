be393136d74c7192896189ad47787b3a4d2c868c
suhelkhanca <suhel.hata@gmail.com>
2025-03-11 04:39:30 +0530

    add last : abort-request when component unmount

--- 08-useEffect-and-fetch-data/01-useEffect/learn-use-effect/src/App.jsx
+++ 08-useEffect-and-fetch-data/01-useEffect/learn-use-effect/src/App.jsx
@@ -1,21 +1,55 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import FetchDataExample from './FetchDataExample';
+import { useEffect, useState } from "react";
+import "./App.css";
+import ExampleUseEffect from "./ExampleUseEffect";
+import FetchDataExample from "./FetchDataExample";
+import FetchData from "./FetchData";
+import CleanupFunctionDemo from "./CleanupFunctionDemo";
+
+import MouseEvent from "./MouseEvent";
+
 const URL = "https://jsonplaceholder.typicode.com/users";
 function App() {
+  const [isShow, setIsShow] = useState(true);
+
   const [users, setUsers] = useState([]);
 
   const fetchData = async () => {
     const response = await fetch(URL);
     const data = await response.json();
     setUsers(data);
-  }
+  };
 
   return (
     <>
-      <FetchDataExample  />
+      {/* <ExampleUseEffect/> */}
+      {/* <FetchData/> */}
+      {/* <FetchDataExample  /> */}
+
+      <label htmlFor="showComponent">Show Component</label>
+      <input
+        type="checkbox"
+        name="showComponent"
+        id="showComponent"
+        checked={isShow}
+        onChange={()=> setIsShow(prevVal => !prevVal)}
+      />
+      {/*{isShow && <CleanupFunctionDemo />} */}
+      
+      {isShow && <FetchDataExample  />}
     </>
-  )
+  );
+
+  // return <>
+  //   <label htmlFor="showComponent">Show Component</label>
+  //     <input
+  //       type="checkbox"
+  //       name="showComponent"
+  //       id="showComponent"
+  //       checked={isShow}
+  //       onChange={()=> setIsShow(prevVal => !prevVal)}
+  //     />
+  //     {isShow && <MouseEvent />}
+  // </>;
 }
 
-export default App
+export default App;
